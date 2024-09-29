23d4dca3c908e996cb461952ad1d8c52bd07a943
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-29 16:40:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function functionName() {
+    alert(arguments.callee.name); // Display the function's own name
+}
+
+// Call the function
+functionName();  // This will alert "functionName"
